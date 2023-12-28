ee17b7ed11c9696219643972912b5044f3bcfb30
Christopher Maltusch <chris.maltusch@coxautoinc.com>
2023-12-28 13:52:49 -0500

    Day 3 Part 1 & 2 Solutions

--- template/download_input.py
+++ template/download_input.py
@@ -3,7 +3,7 @@ import requests
 #this block of code automates the download of the input file; should only be run once not every single time
 
 cookies = {'session': '53616c7465645f5f46485efdb1e993f0b2813dffb3438d335654dbb21ebe0ecb8206e5c5fdcd854605cbd3852f1c753eff3a3c3364341d3dc0d75029609429c6'}
-url = 'https://adventofcode.com/2023/day/1/input'
+url = 'https://adventofcode.com/2023/day/3/input'
 
 response = requests.get(url, cookies=cookies)
 
@@ -14,7 +14,7 @@ else:
 
 #this block of code is used to save the download to a text file
 
-filename = 'advent-of-code/2023/day-1/input.txt'  # Name of the file where you want to save the input
+filename = 'input.txt'  # Name of the file where you want to save the input
 
 with open(filename, 'w') as file:
     if isinstance(input_data, list):
